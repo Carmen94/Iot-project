0837cd1aa52a18aff6754c295e713a2ea514764a
Carmen <riverajc@RIVERAJC2>
2018-10-05 11:35:45 -0500

    layout for Dashboard and main window

--- airsense/routes/index.js
+++ airsense/routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/dashboard', function(req, res, next) {
+  res.render('dashboard', { title: 'Express' });
+});
+
+router.get('/main', function(req, res, next) {
+  res.render('main', { title: 'Express' });
+});
+
+router.post('/login',function(req,res){
+  console.log("Hola");
+  res.render('main',{username:"Carmen"});
+})
+
 module.exports = router;
