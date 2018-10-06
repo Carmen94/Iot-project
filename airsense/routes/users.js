6996af77a0fa7bf5d011f24e18451315c027bfa0
Carmen <riverajc@RIVERAJC2>
2018-10-06 11:47:10 -0500

    Added MSSQL to package

--- airsense/routes/users.js
+++ airsense/routes/users.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var mssql = require('mssql');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+
   res.send('respond with a resource');
 });
 
